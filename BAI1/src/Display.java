36eafa01f7afcb1cb284e255809987cb0e3bf260
Your Name <you@example.com>
2019-06-03 17:08:48 +0700

    Thay doi

--- BAI1/src/Display.java
+++ BAI1/src/Display.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
 public class Display {
-
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap ten:");
+        String name=scanner.nextLine();
+        System.out.println("Hello: "+ name);
+    }
 }
