5dd532f14c1c34b0eae02f7e7b19e6257abb2bc9
Your Name <you@example.com>
2019-06-04 08:18:05 +0700

    Thay doi

--- BAI1/src/BMI.java
+++ BAI1/src/BMI.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class BMI {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Nhap weight:");
-        double weight=scanner.nextDouble();
+        double weight = scanner.nextDouble();
         System.out.println("Nhap height:");
-        double height=scanner.nextDouble();
-        System.out.println("bmi:"+bmi(weight,height));
+        double height = scanner.nextDouble();
+        double bmi = bmi(weight, height);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+
+        if (bmi < 18)
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        else if (bmi < 25.0)
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        else if (bmi < 30.0)
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        else
+            System.out.printf("%-20.2f%s", bmi, "Obese");
     }
-    public static double bmi(double weight, double height){
-        return weight/(height*height);
+
+    public static double bmi(double weight, double height) {
+        return weight / (height * height);
     }
 }
