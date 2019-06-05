b490cda3a04c0d71aa443f11686773a5d16a576b
Your Name <you@example.com>
2019-06-05 09:27:57 +0700

    Thay doi

---
+++ BAI2/src/CountChar.java
@@ -0,0 +1,20 @@
+//import java.util.Scanner;
+//
+//public class CountChar {
+//    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("Nhap chuoi str:");
+//        String str=scanner.nextLine();
+//        System.out.println("Nhap ky tu cha:");
+//       Character cha = scanner.next();
+//    }
+//    public static int countChar(String str,Character cha){
+//        int count=0;
+//        for(int i=0;i<str.length();i++){
+//            if(str.charAt(i)==cha){
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//}
--- BAI2/src/DeleteValue.java
+++ BAI2/src/DeleteValue.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class DeleteValue {
     public static void main(String[] args) {
-        int[]inputArr={1,2,3,4,5,0,0,0,0};
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Nhap index:");
-        int index=scanner.nextInt();
-        int[] newArr=delete(inputArr,index);
-        for (int i=0;i<newArr.length;i++){
-            System.out.print (newArr[i]+" ");
+        int[] inputArr = {1, 2, 3, 4, 5, 6, 7, 0, 0};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap value:");
+        int value = scanner.nextInt();
+        int[] newArr = delete(inputArr, value);
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
         }
     }
-    public static int[] delete(int[]arr,int index){
-       for(int i=index;i<arr.length-1;i++){
-           arr[i]=arr[i+1];
-       }
+
+    public static int[] delete(int[] arr, int value) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value)
+                index = i;
+        }
+        if (index == -1) {
+            return arr;
+        } else {
+            for (int i = index; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+        }
         return arr;
     }
 }
