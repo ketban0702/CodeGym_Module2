f1a210366e8856d63e84571c4b60d38d250871bf
Your Name <you@example.com>
2019-06-05 09:56:27 +0700

    Thay doi

--- BAI2/src/CountChar.java
+++ BAI2/src/CountChar.java
@@ -1,20 +1,22 @@
-//import java.util.Scanner;
-//
-//public class CountChar {
-//    public static void main(String[] args) {
-//        Scanner scanner=new Scanner(System.in);
-//        System.out.println("Nhap chuoi str:");
-//        String str=scanner.nextLine();
-//        System.out.println("Nhap ky tu cha:");
-//       Character cha = scanner.next();
-//    }
-//    public static int countChar(String str,Character cha){
-//        int count=0;
-//        for(int i=0;i<str.length();i++){
-//            if(str.charAt(i)==cha){
-//                count++;
-//            }
-//        }
-//        return count;
-//    }
-//}
+import java.util.Scanner;
+import java.lang.String;
+public class CountChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap chuoi str:");
+        String str = scanner.nextLine();
+        System.out.println("Nhap ky tu cha:");
+        String ch = scanner.next();
+        System.out.println(ch+"xuat hien "+countChar(str,ch.charAt(0))+" lan");
+    }
+
+    public static int countChar(String str, char cha) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == cha) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
