1b95230640cccffb48a2f9d06dc944f55e5b29c7
Your Name <you@example.com>
2019-06-20 14:30:27 +0700

    Thay doi

---
+++ BAI11/thuc hanh 2/src/thuchanh2/LoginServlet.java
@@ -0,0 +1,35 @@
+package thuchanh2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Minh_Nguyen on  6/20/2019
+ * @Project ${NAME}
+ * @Email:
+ */
+@WebServlet(name = "LoginServlet",urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+
+        PrintWriter printWriter =response.getWriter();
+        printWriter.println("<html>");
+        if(("admin".equals(username))&&("admin".equals(password))){
+            printWriter.println("<h1>Welcome  "+  username + "  to Website</h1>");
+        }else {
+            printWriter.println("<h1>Login Error</h1>");
+        }
+        printWriter.println("<html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ BAI11/thuc hanh 2/web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 6/20/2019
+  Time: 11:20 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h1>Login</h1>
+  <form method="post" action="login">
+    User:     <input type="text" name="username"></br></br>
+    Password: <input type="text" name="password"></br></br>
+    <input type="submit">
+  </form>
+
+  </body>
+</html>
